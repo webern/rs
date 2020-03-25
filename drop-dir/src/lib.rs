3d6470fb71fdc74bda5f846655791e8576eb39ba
Matt Briggs <matthew.james.briggs@gmail.com>
2020-03-25 11:41:13 -0700

    drop-dir

---
+++ drop-dir/src/lib.rs
@@ -0,0 +1,77 @@
+// Copyright 2020 by Matthew James Briggs
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+use std::fs;
+use std::ops::Drop;
+use std::path::PathBuf;
+
+/// Represents a Directory that will be automatically deleted when it goes out of scope.
+///
+/// # Example
+///
+/// ```
+/// use std::path::PathBuf;
+/// use drop_dir::DropDir;
+/// use std::fs::File;
+///
+/// let drop_dir = DropDir::new(PathBuf::from("/tmp/some/temp/path")).unwrap();
+/// let mut file = File::create(drop_dir.path().join("file.txt")).unwrap();
+/// // drop_dir deleted when it goes out of scope.
+/// ```
+/// ## Limitation
+///
+/// In the example above, only the last component of the `drop_dir` is removed.
+/// That is, the dir `/tmp/some/temp/path` is deleted, but `/tmp/some/temp` remains.
+/// Any other behavior would get complicated.
+pub struct DropDir {
+    path_buf: PathBuf,
+}
+
+impl Drop for DropDir {
+    fn drop(&mut self) {
+        let result = fs::remove_dir_all(&self.path_buf);
+        if result.is_err() {
+            println!(
+                "Could not delete directory '{}': {}",
+                self.path_buf.to_string_lossy(),
+                result.err().unwrap()
+            );
+        }
+    }
+}
+
+impl DropDir {
+    /// # new
+    ///
+    /// Constructs a new DropDir object from a PathBuf.
+    /// Creates the directory at PathBuf if it does not exist (using `fs::create_dir_all`).
+    ///
+    /// # Example
+    /// ```
+    /// # use std::path::PathBuf;
+    /// # use drop_dir::DropDir;
+    /// # use std::fs::File;
+    /// let drop_dir = DropDir::new(PathBuf::from("/tmp/some/temporary/path")).unwrap();
+    /// ```
+    pub fn new(path_buf: PathBuf) -> Result<DropDir, std::io::Error> {
+        fs::create_dir_all(&path_buf)?;
+        Ok(DropDir { path_buf })
+    }
+
+    /// # path
+    ///
+    /// Returns a clone of the internally held PathBuf.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use std::path::PathBuf;
+    /// # use drop_dir::DropDir;
+    /// # use std::fs::File;
+    /// # let drop_dir = DropDir::new(PathBuf::from("/tmp/some/temporary/path")).unwrap();
+    /// let path_str = drop_dir.path().to_string_lossy();
+    /// ```
+    pub fn path(&self) -> PathBuf {
+        self.path_buf.clone()
+    }
+}

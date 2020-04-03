5e7139c084846eec087182aac7a2600cc777096f
Matt Briggs <matthew.james.briggs@gmail.com>
2020-04-02 19:45:12 -0700

    compiles

--- ezxml/tests/invalid_syntax.rs
+++ ezxml/tests/invalid_syntax.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
+use std::ffi::OsStr;
 use std::fs;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use test_dir;
 
@@ -23,24 +24,66 @@ fn get_files() -> Vec<PathBuf> {
     result
 }
 
+#[derive(Debug, Clone)]
 pub struct TestXmlFile {
+    pub name: String,
     pub xml: PathBuf,
     pub metadata: PathBuf,
 }
 
 pub fn get_xml_files(listing: &Vec<PathBuf>) -> Vec<TestXmlFile> {
     let mut map: HashMap<String, TestXmlFile> = HashMap::new();
-    for x in listing.iter() {}
-    Vec::new()
+    let mut result = Vec::new();
+    for p in listing.iter() {
+        let fname = get_filename(p);
+        let ext = get_ext(p);
+        let mut test_name = "".to_string();
+        let mut is_xml = true;
+        match ext.as_str() {
+            "xml" => {
+                test_name = fname.replace(".xml", "");
+            }
+            "json" => {
+                is_xml = false;
+                let sub = fname.replace(".json", "");
+                let metadata_str = PathBuf::from(&sub).extension().unwrap().to_str().unwrap().to_string();
+                assert!(metadata_str == "metadata".to_string());
+                test_name = sub.replace(".metadata", "")
+            }
+            _ => { panic!("could not parse the path '{}'", p.display()) }
+        }
+        let mut txf = TestXmlFile { name: Default::default(), xml: Default::default(), metadata: Default::default() };
+        if let Some(found) = map.get_mut(&test_name) {
+            txf = found.clone();
+        }
+        txf.name = test_name.clone();
+        if is_xml {
+            txf.xml = p.clone();
+        } else {
+            txf.metadata = p.clone();
+        }
+        map.insert(test_name, txf);
+    }
+    for entry in map.iter() {
+        result.push(entry.1.clone())
+    }
+    result
 }
 
-fn get_extension_from_filename(filename: &str) -> Option<&str> {
-    Path::new(filename)
-        .extension()
-        .and_then(OsStr::to_str)
+fn get_ext<P: AsRef<Path>>(p: P) -> String {
+    Path::new(p.as_ref()).extension().unwrap().to_str().unwrap().to_string()
+}
+
+fn get_filename<P: AsRef<Path>>(p: P) -> String {
+    Path::new(p.as_ref()).file_name().unwrap().to_str().unwrap().to_string()
 }
 
 #[test]
 fn test() {
     let files = get_files();
+    let stuff = get_xml_files(&files);
+    assert!(stuff.len() > 0);
+    for x in stuff.iter() {
+        println!("{:?}", x)
+    }
 }

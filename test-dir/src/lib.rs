a940f6b212de0b33b04d359dbd7387dd09053ed9
Matthew James Briggs <6260372+webern@users.noreply.github.com>
2020-04-05 12:01:59 -0700

    ezxml: very early setup work (#3)

---
+++ test-dir/src/lib.rs
@@ -0,0 +1,39 @@
+use std::path::PathBuf;
+
+// const XML_FILES: &str = "xml-files";
+const XML_DATA: &str = "data";
+
+/// Returns the path to this crate.
+fn self_dir() -> PathBuf {
+    PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .canonicalize()
+        .unwrap()
+}
+
+/// Assumes that we are in a crate within a workspace and returns the path to the workspace dir.
+pub fn workspace_dir() -> PathBuf {
+    let mut p = self_dir();
+    p.pop();
+    p.canonicalize().unwrap()
+}
+
+pub fn xml_data_dir() -> PathBuf {
+    workspace_dir().join(XML_DATA).canonicalize().unwrap()
+}
+
+// pub fn xml_syntax_errors() -> PathBuf {
+//     xml_data_dir().join(XML_SYNTAX_ERRORS).canonicalize().unwrap()
+// }
+
+#[cfg(test)]
+mod tests {
+    use crate::self_dir;
+
+    #[test]
+    fn test_self_dir() {
+        let dir = self_dir();
+        let components = dir.components();
+        let last_component = components.last().unwrap().as_os_str().to_string_lossy();
+        assert!(last_component.as_ref() == "test-dir");
+    }
+}

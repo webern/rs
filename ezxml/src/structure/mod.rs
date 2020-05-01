bb0834eadd87e942838396938b53038c8cbf77e1
Matthew James Briggs <matthew.james.briggs@gmail.com>
2020-04-30 20:33:22 -0700

    boilerplate impls

--- ezxml/src/structure/mod.rs
+++ ezxml/src/structure/mod.rs
@@ -1,7 +1,9 @@
 extern crate env_logger;
 
+use core::fmt;
 use std::cmp::Ordering;
 use std::collections::{HashMap, VecDeque};
+use std::hash::{Hash, Hasher};
 
 #[derive(Debug, Clone, Eq, PartialOrd, PartialEq, Hash, Default)]
 pub struct Name {
@@ -9,36 +11,8 @@ pub struct Name {
     pub name: String,
 }
 
-// #[derive(Debug, Clone, Eq, PartialOrd, PartialEq, Hash, Default)]
-// pub struct Element {
-//     namespace: Option<String>,
-//     name: String,
-//     content: ElementContent,
-// }
-//
-// impl Element {
-//     pub fn name<'a>(&'a self) -> &'a str {
-//         self.name.as_str()
-//     }
-//
-//     pub fn content<'a>(&'a self) -> &'a ElementContent {
-//         &self.content
-//     }
-//
-//     pub fn mut_content<'a>(&'a mut self) -> &'a mut ElementContent {
-//         &mut self.content
-//     }
-//
-//     pub fn text<'a>(&'a self) -> Option<&'a str> {
-//         if let ElementContent::Text(s) = &self.content {
-//             return Some(s.as_str());
-//         }
-//         None
-//     }
-// }
-
 #[derive(Debug, Clone, Eq, PartialOrd, PartialEq, Hash, Default)]
-struct PIData {}
+pub struct PIData {}
 
 #[derive(Debug, Clone, Eq, PartialOrd, PartialEq, Hash, Default)]
 struct Attribute {
@@ -52,7 +26,7 @@ pub struct Nodes(VecDeque<Node>);
 impl Clone for Nodes {
     fn clone(&self) -> Nodes {
         let mut result = VecDeque::new();
-        for &node in self.0.iter() {
+        for node in self.0.iter() {
             result.push_back(node.clone())
         }
         Nodes(result)
@@ -108,30 +82,154 @@ impl PartialOrd for Nodes {
 
     fn lt(&self, other: &Self) -> bool {
         if let Some(ordering) = self.partial_cmp(other) {
-            ordering == Ordering::Less
+            return ordering == Ordering::Less;
         }
         false
     }
 
     fn le(&self, other: &Self) -> bool {
         if let Some(ordering) = self.partial_cmp(other) {
-            ordering == Ordering::Less || ordering == Ordering::Equal
+            return ordering == Ordering::Less || ordering == Ordering::Equal;
         }
         false
     }
 
     fn gt(&self, other: &Self) -> bool {
         if let Some(ordering) = self.partial_cmp(other) {
-            ordering == Ordering::Greater
+            return ordering == Ordering::Greater;
         }
         false
     }
 
     fn ge(&self, other: &Self) -> bool {
         if let Some(ordering) = self.partial_cmp(other) {
-            ordering == Ordering::Greater || ordering == Ordering::Equal
+            return ordering == Ordering::Greater || ordering == Ordering::Equal;
         }
         false
+    }
+}
+
+// TODO - extract key and value types
+pub struct OrdMap(HashMap<String, String>);
+
+impl Clone for OrdMap {
+    fn clone(&self) -> Self {
+        let mut result = HashMap::new();
+        for (k, v) in self.0.iter() {
+            result.insert(k.clone(), v.clone());
+        }
+        Self(result)
+    }
+}
+
+impl Default for OrdMap {
+    fn default() -> Self {
+        Self(HashMap::new())
+    }
+}
+
+impl PartialEq for OrdMap {
+    fn eq(&self, other: &Self) -> bool {
+        if (self.0.len() != other.0.len()) {
+            return false;
+        }
+        for (k, v) in self.0.iter() {
+            if let Some(other_v) = other.0.get(k) {
+                if other_v != v {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+        true
+    }
+}
+
+impl Eq for OrdMap {}
+
+impl OrdMap {
+    pub fn map(&self) -> &HashMap<String, String> {
+        return &self.0;
+    }
+
+    pub fn mut_map(&mut self) -> &mut HashMap<String, String> {
+        return &mut self.0;
+    }
+
+    fn size_le(&self, other: &Self) -> bool {
+        self.0.len() < other.0.len()
+    }
+
+    fn size_gt(&self, other: &Self) -> bool {
+        self.0.len() > other.0.len()
+    }
+}
+
+impl PartialOrd for OrdMap {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        if (self.size_le(other)) {
+            return Some(Ordering::Less);
+        } else if (self.size_gt(other)) {
+            return Some(Ordering::Greater);
+        }
+        for (k, v) in self.0.iter() {
+            if let Some(other_v) = other.0.get(v.as_str()) {
+                if v < other_v {
+                    return Some(Ordering::Less);
+                } else if v > other_v {
+                    return Some(Ordering::Greater);
+                }
+            } else {
+                // we will define the hash map that first has a value that the other one doesn't as
+                // being 'larger'.
+                return Some(Ordering::Greater);
+            }
+        }
+        Some(Ordering::Equal)
+    }
+
+    fn lt(&self, other: &Self) -> bool {
+        if let Some(ordering) = self.partial_cmp(other) {
+            return ordering == Ordering::Less;
+        }
+        false
+    }
+
+    fn le(&self, other: &Self) -> bool {
+        if let Some(ordering) = self.partial_cmp(other) {
+            return ordering == Ordering::Less || ordering == Ordering::Equal;
+        }
+        false
+    }
+
+    fn gt(&self, other: &Self) -> bool {
+        if let Some(ordering) = self.partial_cmp(other) {
+            return ordering == Ordering::Greater;
+        }
+        false
+    }
+
+    fn ge(&self, other: &Self) -> bool {
+        if let Some(ordering) = self.partial_cmp(other) {
+            return ordering == Ordering::Greater || ordering == Ordering::Equal;
+        }
+        false
+    }
+}
+
+impl fmt::Debug for OrdMap {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl Hash for OrdMap {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        for (k, v) in self.0.iter() {
+            k.hash(state);
+            v.hash(state);
+        }
     }
 }
 
@@ -140,7 +238,7 @@ pub struct ElementData {
     pub namespace: Option<String>,
     pub name: String,
     // TODO - introduce Attribute type
-    pub attributes: HashMap<String, String>,
+    pub attributes: OrdMap,
     pub nodes: VecDeque<Node>,
 }
 
@@ -207,8 +305,8 @@ mod tests {
     #[test]
     fn structs_test() {
         init_logger();
-        let mut doc = Document::new();
-        let mut root = doc.mut_root();
-        let mut root_content = root.mut_content();
+        let mut _doc = Document::new();
+        // let mut root = doc.root();
+        // let mut root_content = root.mut_content();
     }
 }

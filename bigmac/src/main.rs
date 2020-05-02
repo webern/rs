d425cd70ccb5dc2310d20b5253fe107179279433
Matt Briggs <brigmatt@amazon.com>
2020-05-02 15:25:44 -0700

    progress on the impossible

--- bigmac/src/main.rs
+++ bigmac/src/main.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 use std::fmt;
+use std::path::PathBuf;
 
 #[derive(Debug)]
 struct MyError {
@@ -11,7 +12,11 @@ struct MyError {
 
 impl fmt::Display for MyError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "SuperError is here!")
+        if let Some(src) = &self.source {
+            write!(f, "{}:{} {}: {}", self.file, self.line, self.message, src.as_ref())
+        } else {
+            write!(f, "{}:{} {}", self.file, self.line, self.message)
+        }
     }
 }
 
@@ -25,7 +30,18 @@ impl Error for MyError {
 }
 
 fn main() {
-    println!("hello");
+    let result = std::fs::read_to_string(PathBuf::from("bad/path/fake/nonono"));
+    // let boxed: Box<dyn std::error::Error> = result.err().unwrap().into();
+    // let e = MyError {
+    //     message: format!("Unable to open fake path '{}'", "bad/path/fake/nonono"),
+    //     file: file!().to_string(),
+    //     line: line!() as u64,
+    //     source: Some(boxed),
+    // };
+
+    let x = wrap!(result.err().unwrap());
+    // println!("{}", e);
+    println!("{}", x);
     // yo!("poo");
     // yall!("poo", "foo", "loo");
 }
@@ -38,9 +54,37 @@ fn main() {
 // }
 //
 //
-// #[macro_export]
-// macro_rules! yall {
-//   (($name:expr),*) => {
-//      println!("Yo {}!", $name)
-//   };
+#[macro_export]
+macro_rules! wrap {
+    // Base case:
+    ($err:expr) => (MyError {
+        message: "poo poo".to_string(),
+        file: file!().to_string(),
+        line: line!() as u64,
+        source: Some($err.into()),
+    });
+        // let boxed: Box<dyn std::error::Error> = $err.into();
+        // return MyError {
+        //     message: format!("some message here"),
+        //     file: file!().to_string(),
+        //     line: line!() as u64,
+        //     source: Some(boxed),
+        // }
+    // );
+    // // `$x` followed by at least one `$y,`
+    // ($x:expr, $($y:expr),+) => (
+    //     // Call `find_min!` on the tail `$y`
+    //     std::cmp::min($x, find_min!($($y),+))
+    // )
+}
+
+// fn create_error<E>(source: E, message: &str, file: &str, line: u64) -> MyError
+//     where E: std::error::Error, {
+//     let boxed: Box<dyn std::error::Error> = source.into();
+//     MyError {
+//         message: message.to_string(),
+//         file: file.to_string(),
+//         line,
+//         source: Some(boxed),
+//     }
 // }

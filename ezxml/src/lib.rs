c815502327ca5456f063bc36a531897c88a896c5
Matthew James Briggs <matthew.james.briggs@gmail.com>
2020-05-03 08:26:01 -0700

    fix typo

--- ezxml/src/lib.rs
+++ ezxml/src/lib.rs
@@ -6,9 +6,9 @@ Half baked work on XML in Rust.
  * [x] xdoc: create the ezfile in a test using structs
  * [x] xdoc: write assertions for the ezfile structs
  * [ ] xdoc: serialize the ezfile to xml
- * [ ] xdoc: assert serialized xml equals a string contstant of the xml
+ * [ ] xdoc: assert serialized xml equals a string constant of the xml
  * [ ] xdoc: serialize the ezfile to json
- * [ ] xtest: add the serialize ezfile data to the metadata file as an assertion.
+ * [ ] xtest: add the serialized ezfile data to the metadata file as an assertion.
  * [ ] ezxml: generate an assertion of the ezfile using build.rs
  * [ ] ezxml: make the parser work so that the ezfile test passes
 */

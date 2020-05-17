059f6d067cd9cb1958a6d5968b88d7a2704e647c
Matthew James Briggs <mjb@bitflip.software>
2020-05-16 22:00:18 -0700

    ezxml: pre-mvp parser (#5)
    * simple xml structs
    
    * parser beginnings
    
    * parser state struct
    
    * whatever progress there is
    
    * gitignore
    
    * just more mess
    
    * where we left off
    
    * xml files
    
    * broken
    
    * no compile
    
    * compiles
    
    * work on test generation
    
    * buildrs
    
    * open_xml_file
    
    * serde
    
    * syntax failure test generation
    
    * baby tests passing
    
    * clippy
    
    * github
    
    * output clippy version
    
    * clippy
    
    * hide document members
    
    * ignore attributes for now
    
    * impl
    
    * another file
    
    * boilerplate impls
    
    * cleanup and test stubbing
    
    * create xml struct crate
    
    * use xml struct crate
    
    * mostly things are good
    
    * fix build.rs
    
    * begin work on json representation
    
    * more serde
    
    * todos
    
    * structs
    
    * cleanup
    
    * rename to xdoc
    
    * finish xdoc rename
    
    * rename xtest and finish xdoc rename
    
    * configs
    
    * configs
    
    * savepoint
    
    * rust is so fucking hard
    
    * this compiles
    
    * using a writer
    
    * first written xml document
    
    * basic pretty print
    
    * fix typo
    
    * fix typo
    
    * basic xml declaration
    
    * fiddling with newlines mostly
    
    * passing tests
    
    * clippy and fmt
    
    * clippy
    
    * update todo readme
    
    * clippy is angry
    
    * manual fmt
    
    * cargo fmt
    
    * snake case
    
    * start moving things in xtest
    
    * cleanup in xtest
    
    * test gen work
    
    * create assertion for good xml docs
    
    * readme todos
    
    * parser hell does not work
    
    * char ranges
    
    * chars
    
    * pi parse
    
    * pi_data
    
    * add a stack to the parser
    
    * parse xml declaration very basic
    
    * fix return
    
    * fix string matches
    
    * cleaner function responsibility
    
    * cleanup and refactor
    
    * churn to consolidate parser state and iter
    
    * parse the element name
    
    * pre-mvp - minimal working parser
    
    * delete stuped idea file
    
    * gitignore
    
    * fmt
    
    * clippy-not-done
    
    * clippy
    
    * clippy tests
    
    * clippy tests
    
    * xml
    
    * Update ezxml/README.md

--- ds/src/lib.rs
+++ ds/src/lib.rs
@@ -1,34 +1,88 @@
 use std::cmp::Ordering;
-use std::collections::VecDeque;
 use std::hash::{Hash, Hasher};
 
-use crate::node::Node;
-
 #[derive(Debug, Eq)]
-#[cfg_attr(
-    feature = "serde",
-    derive(Serialize, Deserialize),
-    serde(rename_all = "snake_case")
-)]
-pub struct Nodes(VecDeque<Node>);
+pub struct Stack<T>(Vec<T>)
+where
+    T: Clone + Eq + PartialEq + PartialOrd + Hash;
 
-impl Clone for Nodes {
-    fn clone(&self) -> Nodes {
-        let mut result = VecDeque::new();
-        for node in self.0.iter() {
-            result.push_back(node.clone())
+impl<T> Stack<T>
+where
+    T: Clone + Eq + PartialEq + PartialOrd + Hash,
+{
+    pub fn new() -> Self {
+        Stack(Vec::new())
+    }
+
+    pub fn from(data: Vec<T>) -> Self {
+        Stack(data)
+    }
+
+    pub fn push(&mut self, value: T) {
+        self.0.push(value);
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        if self.0.is_empty() {
+            return None;
         }
-        Nodes(result)
+        Some(self.0.remove(self.0.len() - 1))
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        if self.0.is_empty() {
+            return None;
+        }
+        self.0.last()
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        if self.0.is_empty() {
+            return None;
+        }
+        let i = self.0.len() - 1;
+        self.0.get_mut(i)
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+
+    pub fn vec(&self) -> &Vec<T> {
+        &self.0
+    }
+
+    pub fn vec_mut(&mut self) -> &mut Vec<T> {
+        &mut self.0
     }
 }
 
-impl Default for Nodes {
-    fn default() -> Nodes {
-        Nodes(VecDeque::new())
+impl<T: Clone> Clone for Stack<T>
+where
+    T: Clone + Eq + PartialEq + PartialOrd + Hash,
+{
+    fn clone(&self) -> Self {
+        let mut result: Vec<T> = Vec::new();
+        for item in self.0.iter() {
+            result.push(item.clone())
+        }
+        Self(result)
     }
 }
 
-impl PartialEq for Nodes {
+impl<T> Default for Stack<T>
+where
+    T: Clone + Eq + PartialEq + PartialOrd + Hash,
+{
+    fn default() -> Self {
+        Self(Vec::new())
+    }
+}
+
+impl<T: Eq> PartialEq for Stack<T>
+where
+    T: Clone + Eq + PartialEq + PartialOrd + Hash,
+{
     fn eq(&self, other: &Self) -> bool {
         if self.0.len() != other.0.len() {
             return false;
@@ -42,7 +96,10 @@ impl PartialEq for Nodes {
     }
 }
 
-impl Nodes {
+impl<T> Stack<T>
+where
+    T: Clone + Eq + PartialEq + PartialOrd + Hash,
+{
     fn size_le(&self, other: &Self) -> bool {
         self.0.len() < other.0.len()
     }
@@ -52,7 +109,10 @@ impl Nodes {
     }
 }
 
-impl PartialOrd for Nodes {
+impl<T> PartialOrd for Stack<T>
+where
+    T: Clone + Eq + PartialEq + PartialOrd + Hash,
+{
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         if self.size_le(other) {
             return Some(Ordering::Less);
@@ -98,10 +158,13 @@ impl PartialOrd for Nodes {
     }
 }
 
-impl Hash for Nodes {
+impl<T> Hash for Stack<T>
+where
+    T: Clone + Eq + PartialEq + PartialOrd + Hash,
+{
     fn hash<H: Hasher>(&self, state: &mut H) {
-        for node in self.0.iter() {
-            node.hash(state);
+        for item in self.0.iter() {
+            item.hash(state);
         }
     }
 }
